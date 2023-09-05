0c4289e593444674164822b2e48b7fdc77c63f27
이의석 <euisuk.lee@kurlycorp.com>
2023-09-05 09:20:17 +0900

    fix: BaseViewController reference issue

---
+++ AKKIN/AKKIN/Presentation/Base/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  AKKIN
+//
+//  Created by MK-Mac-413 on 2023/09/04.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureSubviews()
+        makeConstraints()
+        view.backgroundColor = .white
+    }
+    
+    // MARK: Configuration
+    func configureSubviews() {
+        
+    }
+    
+    // MARK: Layout
+    func makeConstraints() {
+    }
+}
