645263355d9de48de7a6464b04cf56c80b809ad0
Bibi-urssu <bibi.urssu@gmail.com>
2023-09-18 21:11:40 +0900

    refactor: 메인 화면 UI 수정

--- AKKIN/AKKIN/Presentation/Main/MainCardCollectionView.swift
+++ AKKIN/AKKIN/Presentation/Main/MainCardCollectionView.swift
@@ -10,6 +10,15 @@ import UIKit
 final class MainCardCollectionView: BaseView {
     
     // MARK: UI Components
+    private let akkinLabel = UILabel().then {
+        $0.text = "오늘 얼마 아낀거지?"
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+    }
+
+    private let addButton = BaseButton().then {
+        $0.setImage(UIImage(named: "addButton"), for: .normal)
+    }
+
     public lazy var cardCollectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: createLayout())
         collectionView.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.99, alpha: 1)
@@ -21,14 +30,14 @@ final class MainCardCollectionView: BaseView {
     private func createLayout() -> UICollectionViewCompositionalLayout {
         let item = NSCollectionLayoutItem(
             layoutSize: .init(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .absolute(223)))
+                widthDimension: .absolute(222),
+                heightDimension: .absolute(262)))
         item.contentInsets = .init(top: 0, leading: 7, bottom: 0, trailing: 7)
 
         let group = NSCollectionLayoutGroup.horizontal(
             layoutSize: .init(
-                widthDimension: .fractionalWidth(0.9),
-                heightDimension: .absolute(223)), subitems: [item])
+                widthDimension: .fractionalWidth(0.57),
+                heightDimension: .absolute(262)), subitems: [item])
 
         let section = NSCollectionLayoutSection(group: group)
         section.orthogonalScrollingBehavior = .groupPagingCentered
@@ -47,6 +56,8 @@ final class MainCardCollectionView: BaseView {
         super.configureSubviews()
         cardCollectionView.dataSource = self
 
+        addSubview(akkinLabel)
+        addSubview(addButton)
         addSubview(cardCollectionView)
     }
     
@@ -54,9 +65,20 @@ final class MainCardCollectionView: BaseView {
     override func makeConstraints() {
         super.makeConstraints()
 
+        akkinLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(13)
+            $0.leading.equalToSuperview().inset(24)
+        }
+
+        addButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(18)
+            $0.centerY.equalTo(akkinLabel.snp.centerY)
+        }
+
         cardCollectionView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
-            $0.height.equalTo(255)
+            $0.top.equalToSuperview().inset(40)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(295)
         }
     }
     
