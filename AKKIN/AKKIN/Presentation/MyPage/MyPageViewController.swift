b4d6867b8038ecefb0923353eaadf925e2f25789
Bibi-urssu <bibi.urssu@gmail.com>
2023-09-19 23:13:46 +0900

    feat: nickName 수정 기능

--- AKKIN/AKKIN/Presentation/MyPage/MyPageViewController.swift
+++ AKKIN/AKKIN/Presentation/MyPage/MyPageViewController.swift
@@ -11,6 +11,7 @@ final class MyPageViewController: BaseViewController {
     private let headerTitle = ["앱 정보", "계정 관리"]
     private let appInfo = ["서비스 이용약관", "개인 정보 처리 방침", "오픈소스 사용정보"]
     private let account = ["로그아웃", "회원탈퇴"]
+    private var nickName = "거지"
 
     // MARK: UI Components
     private let profileImageView = UIImageView().then {
@@ -20,9 +21,16 @@ final class MyPageViewController: BaseViewController {
         $0.layer.borderColor = UIColor(red: 0.14, green: 0.68, blue: 0.37, alpha: 1).cgColor
     }
 
-    private let nameLabel = UILabel().then {
-        $0.text = "이름이 들어갑니다"
+    private let nickNameLabel = UILabel().then {
         $0.font = UIFont.systemFont(ofSize: 20)
+    }
+
+    private let nickNameTextField = UITextField().then {
+        $0.font = UIFont.systemFont(ofSize: 20)
+        $0.isHidden = true
+        $0.clearButtonMode = .whileEditing
+        $0.tintColor = UIColor(red: 0.14, green: 0.68, blue: 0.37, alpha: 1)
+        $0.underlined(viewSize: 200, color: .black)
     }
 
     private let editButton = BaseButton().then {
@@ -40,6 +48,13 @@ final class MyPageViewController: BaseViewController {
         $0.setImage(UIImage(named: "backButton"), for: .normal)
     }
 
+    private let confirmButton = BaseButton().then {
+        $0.setTitle("완료", for: .normal)
+        $0.setTitleColor(UIColor(red: 0.14, green: 0.68, blue: 0.37, alpha: 1), for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 14)
+        $0.isHidden = true
+    }
+
     // MARK: Environment
     private let router = ExampleRouter()
 
@@ -47,6 +62,7 @@ final class MyPageViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationItem()
+        getNickName()
 
         router.viewController = self
     }
@@ -59,7 +75,8 @@ final class MyPageViewController: BaseViewController {
         myPageTableView.delegate = self
 
         view.addSubview(profileImageView)
-        view.addSubview(nameLabel)
+        view.addSubview(nickNameLabel)
+        view.addSubview(nickNameTextField)
         view.addSubview(editButton)
         view.addSubview(myPageTableView)
 
@@ -67,11 +84,48 @@ final class MyPageViewController: BaseViewController {
             guard let self else { return }
             router.dismissViewController()
         }
+
+        editButton.tap = { [weak self] in
+            guard let self else { return }
+            self.editButtonTapped()
+        }
+
+        confirmButton.tap = { [weak self] in
+            guard let self else { return }
+            self.confirmButtonTapped()
+        }
+    }
+
+    private func getNickName() {
+        nickNameLabel.text = nickName
+        nickNameTextField.placeholder = nickName
+    }
+ 
+    private func editButtonTapped() {
+        editButton.isHidden = true
+        confirmButton.isHidden = false
+        nickNameLabel.isHidden = true
+        nickNameTextField.isEnabled = true
+        nickNameTextField.isHidden = false
+        nickNameTextField.text = nickNameLabel.text
+    }
+
+    private func confirmButtonTapped() {
+        editButton.isHidden = false
+        confirmButton.isHidden = true
+        nickNameLabel.isHidden = false
+        nickNameTextField.isEnabled = false
+        nickNameTextField.isHidden = true
+        if nickNameTextField.text != "" {
+        nickName = nickNameTextField.text ?? nickName
+        }
+        getNickName()
     }
 
     private func setNavigationItem() {
         navigationItem.title = "마이페이지"
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: confirmButton)
     }
 
     // MARK: Layout
@@ -84,14 +138,20 @@ final class MyPageViewController: BaseViewController {
             $0.width.height.equalTo(140)
         }
 
-        nameLabel.snp.makeConstraints {
+        nickNameLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).inset(177)
             $0.centerX.equalToSuperview().offset(-2)
         }
 
+        nickNameTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(177)
+            $0.centerX.equalToSuperview().offset(4)
+            $0.width.equalTo(155)
+        }
+
         editButton.snp.makeConstraints {
-            $0.leading.equalTo(nameLabel.snp.trailing).offset(2)
-            $0.centerY.equalTo(nameLabel.snp.centerY)
+            $0.leading.equalTo(nickNameLabel.snp.trailing).offset(2)
+            $0.centerY.equalTo(nickNameLabel.snp.centerY)
             $0.width.height.equalTo(20)
         }
 
@@ -155,5 +215,16 @@ extension MyPageViewController: UITableViewDataSource, UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 73
+    }
+}
+
+extension UITextField {
+    func underlined(viewSize: CGFloat, color: UIColor){
+        let border = CALayer()
+        let width = CGFloat(0.34)
+        border.borderColor = color.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height + 28, width: viewSize - 48, height: width)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
     }
 }
