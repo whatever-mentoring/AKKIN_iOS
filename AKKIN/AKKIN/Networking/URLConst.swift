b2707562bbf80451882a35b62fcb5d8ca0ab615d
Bibi-urssu <bibi.urssu@gmail.com>
2023-09-24 17:41:44 +0900

    feat: URLConst

---
+++ AKKIN/AKKIN/Networking/URLConst.swift
@@ -0,0 +1,19 @@
+//
+//  URLConst.swift
+//  AKKIN
+//
+//  Created by 박지윤 on 2023/09/24.
+//
+
+enum URLConst {
+    // MARK: - baseURL
+    static let base = "https://www.seuleuleug.site/api"
+    
+    // MARK: - AppInfoURL
+    static let termsURL = "https://tundra-operation-2d1.notion.site/4448021736b34742bd85202a1d7e2f8e?pvs=25"
+    static let privacyPolicyURL = "https://tundra-operation-2d1.notion.site/3575a8becd474c6a93e066b8c442471d"
+    static let openSourceURL = "https://tundra-operation-2d1.notion.site/b51ed92b2e7b4e09a6a1e4a80d112b2d?pvs=25"
+    
+    // MARK: - Login
+    static let dummyLogin = "/login/dummy/101"
+}
