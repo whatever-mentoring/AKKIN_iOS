c827228211972bd11267e712dba628f3131cd49e
Bibi-urssu <bibi.urssu@gmail.com>
2023-09-24 17:58:56 +0900

    refactor: TargetType 수정

--- AKKIN/AKKIN/Networking/TargetType+Extension.swift
+++ AKKIN/AKKIN/Networking/TargetType+Extension.swift
@@ -11,10 +11,15 @@ import Moya
 
 extension TargetType {
     var baseURL: URL {
-        URL(string: "")!
+        URL(string: URLConst.base)!
     }
     
     var headers: [String : String]? {
+//        let header = [
+//            "Content-Type": "application/json",
+//            "Authorization": "accessToken"
+//        ]
+//        return header
         nil
     }
 }
