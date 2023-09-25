e28f86290817ebe048c2e9b4ef0ec2c8be08bc05
Bibi-urssu <bibi.urssu@gmail.com>
2023-09-26 02:27:15 +0900

    feat: postAkkin parameters

--- AKKIN/AKKIN/Networking/Akkin/AkkinAPI.swift
+++ AKKIN/AKKIN/Networking/Akkin/AkkinAPI.swift
@@ -10,7 +10,16 @@ import Foundation
 
 enum AkkinAPI {
     case getAkkin
-    case postAkkin
+    case postAkkin(
+        year: Int,
+        month: Int,
+        day: Int,
+        category: String,
+        saveContent: String,
+        how: String,
+        expectCost: Int,
+        realCost: Int
+    )
     case patchAkkin
     case deleteAkkin
 }
@@ -46,8 +55,26 @@ extension AkkinAPI: TargetType {
         switch self {
         case .getAkkin:
             return .requestPlain
-        case .postAkkin:
-            return .requestPlain
+        case .postAkkin(
+            let year,
+            let month,
+            let day,
+            let category,
+            let saveContent,
+            let how,
+            let expectCost,
+            let realCost
+        ):
+            return .requestParameters(parameters: [
+                "year": year,
+                "month": month,
+                "day": day,
+                "category": category,
+                "saveContent": saveContent,
+                "how": how,
+                "expectCost": expectCost,
+                "realCost": realCost
+            ], encoding: JSONEncoding.default)
         case .patchAkkin:
             return .requestPlain
         case .deleteAkkin:
