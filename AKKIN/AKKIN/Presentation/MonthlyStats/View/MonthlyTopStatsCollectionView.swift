daf3ec1fa38fcc00a20fc8b773de1374e1715fe5
JJongW <shinjw4675@gmail.com>
2023-10-20 16:35:00 +0900

    feat: MonthlyStats 상세뷰 구현

---
+++ AKKIN/AKKIN/Presentation/MonthlyStats/View/MonthlyTopStatsCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  MonthlyTopStatsCollectionView.swift
+//  AKKIN
+//
+//  Created by 신종원 on 2023/10/20.
+//
+
+import Foundation
---
+++ AKKIN/AKKIN/Presentation/MonthlyStats/View/MonthlyTopStatsListCell.swift
@@ -0,0 +1,8 @@
+//
+//  MonthlyTopStatsListCell.swift
+//  AKKIN
+//
+//  Created by 신종원 on 2023/10/20.
+//
+
+import Foundation
