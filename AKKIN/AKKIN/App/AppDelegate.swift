fb3c3cd9726e5f7da77cd8e5b82c2e8bad2bd445
JJongW <shinjw4675@gmail.com>
2024-01-13 23:56:51 +0900

    fix: AppDelegate Kakao Key 수정

--- AKKIN/AKKIN/App/AppDelegate.swift
+++ AKKIN/AKKIN/App/AppDelegate.swift
@@ -13,8 +13,7 @@ import KakaoSDKCommon
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        KakaoSDK.initSDK(appKey: "{NATIVE_APP_KEY}")
-        // ex) KakaoSDK.initSDK(appKey: "123456789")
+        KakaoSDK.initSDK(appKey: "1a21d7c73dbf14515468d75c515372b1")
         return true
     }
 }
